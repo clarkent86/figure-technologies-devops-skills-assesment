cd0f6aca191f424b48813891755efa729d88ccf5
Joey.Ross <jayr86@gmail.com>
2025-07-14 09:20:01 -0500

    adding parralel test execution

--- restart/restart_test.go
+++ restart/restart_test.go
@@ -77,6 +77,7 @@ func TestRestartDatabases(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			// Create fake clientset
 			cs := fake.NewSimpleClientset()
 
